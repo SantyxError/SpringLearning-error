18e07fb82ad989a34f64592d70027b630e0220f2
Santiago <sanbemal@gmail.com>
2023-08-16 08:12:12 +0200

    107. Creando nueva aplicación y controlador

---
+++ src/main/java/com/bolsadeideas/springnboot/error/app/SpringBootError.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springnboot.error.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootError {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootError.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolsadeideas/springnboot/error/app/PatataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springnboot.error.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
