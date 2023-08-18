5efbc658715b47ec4057957d970eca685fc286fb
Santiago <sanbemal@gmail.com>
2023-08-18 08:53:51 +0200

    114. Lanzamiento de exepción usando Api Optional de Java 8

--- src/main/java/com/bolsadeideas/springnboot/error/app/services/UsuarioService.java
+++ src/main/java/com/bolsadeideas/springnboot/error/app/services/UsuarioService.java
@@ -3,9 +3,12 @@ package com.bolsadeideas.springnboot.error.app.services;
 import com.bolsadeideas.springnboot.error.app.models.domain.Usuario;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioService {
 	public List<Usuario> listar();
 
 	public Usuario obtenerPorId(Integer id);
+
+	public Optional<Usuario> obtenerPorIdOptional(Integer id);
 }
